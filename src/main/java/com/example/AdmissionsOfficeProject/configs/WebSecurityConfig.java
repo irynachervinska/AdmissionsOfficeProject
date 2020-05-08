c316735aa2f8b970e4dbed4af8713c56e4c01403
Cherina97 <Cherina97@gmail.com>
2020-05-08 13:11:26 +0300

    add js for ignoring security

--- src/main/java/com/example/AdmissionsOfficeProject/configs/WebSecurityConfig.java
+++ src/main/java/com/example/AdmissionsOfficeProject/configs/WebSecurityConfig.java
@@ -58,6 +58,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/css/**");
+        web.ignoring().antMatchers("/css/**", "/js/**");
     }
 }
